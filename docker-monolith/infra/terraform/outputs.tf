4a175e078d39a19672abfe3d88a7f29c14d3d2ae
Artem Ustinov <artem198315@gmail.com>
2019-05-29 02:25:16 +0400

    HW 20

--- docker-monolith/infra/terraform/outputs.tf
+++
@@ -1,3 +0,0 @@
-output docker-instances {
-  value = "${google_compute_instance.docker.*.network_interface.0.access_config.0.nat_ip}"
-}
--- docker-monolith/infra/terraform/project.tf
+++
@@ -1,4 +0,0 @@
-resource "google_compute_project_metadata_item" "ssh-keys" {
-  key   = "ssh-keys"
-  value = "shinta:${file("${var.public_key_path}")}appuser:${file("${var.public_key_path}")}appuser2:${file("${var.public_key_path}")}"
-}
